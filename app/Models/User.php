ebe4fbaa8812569ce5e3b14fddcb5b0339118352
takuya-7 <ts.procd@gmail.com>
2022-01-31 15:16:10 +0900

    userモデル、stepモデルへのリレーション作成

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // Stepモデルへのリレーション作成
+    public function steps()
+    {
+        return $this->hasMany('App\Models\Step');
+    }
 }
