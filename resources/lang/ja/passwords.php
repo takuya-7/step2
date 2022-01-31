332fea3d59ca53a5002e2d0ae7a8d4dd40d28ad4
takuya-7 <ts.procd@gmail.com>
2022-01-31 16:57:41 +0900

    エラーメッセージ等の日本語ファイル追加

---
+++ resources/lang/ja/passwords.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    | パスワードリセット言語
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'パスワードがリセットされました',
+    'sent' => 'パスワードリセットリンクがメールで送信されました',
+    'throttled' => '再試行するまでお待ちください',
+    // 'password' => 'パスワードは６字以上、かつ、確認用と一致している必要があります',
+    'token' => 'このパスワードリセットトークンは無効です',
+    'user' => "そのメールアドレスのユーザーは存在しません",
+
+];
