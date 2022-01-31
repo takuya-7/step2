a936142b12655c541b21b78a041f87edbfc7f57f
takuya-7 <ts.procd@gmail.com>
2022-01-31 15:16:58 +0900

    モデル作成

---
+++ app/Models/ChildStep.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ChildStep extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'order',
+        'title',
+        'estimated_achievement_day',
+        'estimated_achievement_hour',
+        'description',
+        'step_id',
+        'delete_flg',
+    ];
+
+    // Stepモデルへのリレーション作成
+    public function user()
+    {
+        return $this->belongsTo('App\Models\Step');
+    }
+}
---
+++ app/Models/Step.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Step extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'estimated_achievement_day',
+        'estimated_achievement_hour',
+        'description',
+        'category_id',
+        'delete_flg',
+    ];
+
+    // Userモデルへのリレーション作成
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+    // Categoryモデルへのリレーション作成
+    public function category()
+    {
+        return $this->belongsTo('App\Models\Category');
+    }
+    // ChildStepモデルへのリレーション作成
+    public function childStep()
+    {
+        return $this->hasMany('App\Models\ChildStep');
+    }
+}
