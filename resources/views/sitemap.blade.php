25f6f2dc32e3d3746f97f8f47016fd29d1fc1225
takuya-7 <ts.procd@gmail.com>
2022-01-31 16:02:05 +0900

    サイトマップ作成

---
+++ resources/views/sitemap.blade.php
@@ -0,0 +1,26 @@
+<!-- レイアウト読み込み -->
+@extends('layouts.layout')
+
+<!-- title設定 -->
+@section('title', 'サイトマップ')
+
+@section('content')
+    <div class="l-content-wrapper">
+        <div class="l-container">
+          <div class="l-content">
+            <div class="l-inner-container">
+
+              <h2>サイトマップ</h2>
+
+              <ul>
+                <li><a href="/">HOME</a></li>
+                <li><a href="{{ route('register') }}">ユーザー登録</a></li>
+                <li><a href="{{ route('login') }}">ログイン</a></li>
+                <li><a href="{{ route('steps') }}">STEP一覧</a></li>
+              </ul>
+              
+            </div>
+          </div>
+        </div>
+    </div>
+@endsection
