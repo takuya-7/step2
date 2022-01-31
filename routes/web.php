3d72014f83cb8e929885bd7fdd478dcac62a322f
takuya-7 <ts.procd@gmail.com>
2022-01-31 16:11:01 +0900

    ルート作成

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,45 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// トップページ
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::group(['middleware' => 'auth'], function() {
+    // マイページ
+    Route::get('/mypage', [App\Http\Controllers\UsersController::class, 'mypage'])->name('mypage');
+    // プロフィール登録・編集
+    Route::get('/profile/edit', [App\Http\Controllers\UsersController::class, 'edit'])->name('profile.edit');
+    // パスワード変更
+    Route::get('/profile/password', [App\Http\Controllers\UsersController::class, 'updatePassword'])->name('profile.password');
+
+    // STEP投稿画面
+    Route::get('/steps/new', [App\Http\Controllers\StepsController::class, 'new'])->name('steps.new');
+    // STEP登録処理
+    Route::post('/steps', [App\Http\Controllers\StepsController::class, 'create'])->name('steps.create');
+    // STEP編集
+    Route::get('/steps/{id}/edit', [App\Http\Controllers\StepsController::class, 'edit'])->name('steps.edit');
+});
+
+// STEP一覧
+Route::get('/steps', [App\Http\Controllers\StepsController::class, 'index'])->name('steps');
+// STEP詳細
+Route::get('/steps/{id}', [App\Http\Controllers\StepsController::class, 'show'])->name('steps.show');
+// 子STEP詳細
+Route::get('/steps/{id}/{order}', [App\Http\Controllers\StepsController::class, 'showChild'])->name('steps.showChild');
+
+// サイトマップ
+Route::get('/sitemap', function () {
+    return view('sitemap');
+})->name('sitemap');
+
+
+// デフォルト設定↓
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
