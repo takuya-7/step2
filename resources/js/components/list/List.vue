498c02012ec134f5aca863f834bf1ef37e3f134f
takuya-7 <ts.procd@gmail.com>
2022-02-01 13:13:30 +0900

    STEP一覧のリストコンポーネント作成

---
+++ resources/js/components/list/List.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <ul class="p-step-list">
+      <li v-for="step in steps" :key="step.id">
+        <ListItem :step='step' :categories='categories'></ListItem>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import { ref } from "vue";
+  import ListItem from './ListItem.vue'
+
+  export default {
+    components: {
+      ListItem,
+    },
+    props: ['steps', 'categories'],
+
+  }
+</script>   
---
+++ resources/js/components/list/ListItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <a :href="stepURL" class="c-card c-card--link c-card--shadow">
+    <div class="c-card__header">
+      <h2 class="c-card__title u-fw-bold">{{ step.title }}</h2>
+      <span class="c-card__header__item">カテゴリ：{{  categories[step.category_id - 1]['name'] }}</span>
+      <span class="c-card__header__item">所要時間：{{ step.estimated_achievement_day }}日 {{ step.estimated_achievement_hour }}時間</span>
+    </div>
+
+    <div class="c-card__content">
+      <p class="c-card__content__p">{{ step.description }}</p>
+    </div>
+  </a>
+</template>
+
+<script>
+  import { ref } from "vue";
+  export default {
+    props: ['step', 'categories'],
+
+    computed: {
+      stepURL: function(){
+        return '/steps/' + this.step.id
+      }
+    },
+
+  }
+</script>
