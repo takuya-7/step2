c20c62690e39d50d0d4e037a9efa4687fe58fa1e
takuya-7 <ts.procd@gmail.com>
2022-02-07 18:09:03 +0900

    子ステップ画面構築

--- resources/views/steps/child-step.blade.php
+++ resources/views/steps/child-step.blade.php
@@ -10,7 +10,31 @@
           <div class="l-content">
             <div class="l-inner-container">
 
-              小STEP詳細
+              <div class="p-step-content">
+                <h1>{{ $step->title }}</h2>
+
+                <div class="u-text-gray u-mb-4">
+                  <time datetime="{{ $created_at }}">投稿：{{ $created_at }}　</time>
+                  <time datetime="{{ $updated_at }}">更新：{{ $updated_at }}</time>
+                </div>
+
+                <div class="u-mb-4">
+                  <div class="u-mb-2">カテゴリー：{{ $category }}</div>
+                  <div>所要時間：{{ $step->estimated_achievement_day }}日 {{ $step->estimated_achievement_hour }}時間</div>
+                </div>
+
+                <h2>概要</h2>
+                <p>{{ $step->description }}</p>
+
+                <h2>STEP{{ $child_step[0]['order'] }}：{{ $child_step[0]['title'] }}</h2>
+                <p>{{ $child_step[0]['description'] }}</p>
+
+                <form method="POST" action="{{ route('steps.registChallenge') }}">
+                  <input type="hidden" name="id" value="{{ $step->id }}">
+                  <button type="submit" class="c-button c-button--blue c-button--width100">このSTEPにチャレンジする</button>
+                </form>
+                
+              </div>
               
             </div>
           </div>
