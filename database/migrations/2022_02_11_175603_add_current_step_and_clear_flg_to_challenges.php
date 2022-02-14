1563c332389f54695dff2331c0ed92735625d88f
takuya-7 <ts.procd@gmail.com>
2022-02-14 22:52:33 +0900

    現在のステップ、クリアフラグのカラムをchallengeテーブルに追加

---
+++ database/migrations/2022_02_11_175603_add_current_step_and_clear_flg_to_challenges.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCurrentStepAndClearFlgToChallenges extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('challenges', function (Blueprint $table) {
+            // レコード削除
+            DB::statement('DELETE FROM challenges');
+            // カラム追加
+            $table->integer('current_step')->default(1);;
+            $table->integer('clear_flg')->default(0);;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('challenges', function (Blueprint $table) {
+            // カラム削除
+            $table->dropColumn('current_step');
+            $table->dropColumn('clear_flg');
+        });
+    }
+}
