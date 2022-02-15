19f453f99bd8979a0852c257101d4c127cf7f789
takuya-7 <ts.procd@gmail.com>
2022-02-15 13:06:08 +0900

    登録済みSTEP、チャレンジ中STEP表示実装

--- resources/views/profile/mypage.blade.php
+++ resources/views/profile/mypage.blade.php
@@ -7,27 +7,53 @@
 @section('content')
 <div class="l-content-wrapper">
   <div class="l-container">
-    <div class="row">
-      <h1 class="c-page-title">マイページ</h1>
+    <div class="">
+      <h1 class="c-page-title u-mb-5">マイページ</h1>
 
-      <div class="profile-container col-md-8">
+      <div class="">
 
-        <a href="{{ route('steps.new') }}">STEP投稿</a>
+        <div class="u-mb-5">
+          <a href="{{ route('steps.new') }}" class="c-button c-button--blue c-button--width100">STEPを投稿する</a>
+        </div>
         
-        <h2>登録済みSTEP</h2>
-        <ul>
-          <li>STEP1</li>
-          <li>STEP2</li>
-        </ul>
+        <section class="u-mb-4">
+          <h2>登録済みSTEP</h2>
+          <ul>
+            @foreach($registered_steps as $registered_step)
+              <li class="">
+                <a href="/steps/{{ $registered_step->id }}">
+                  {{ $registered_step['title'] }}
+                </a>
+              </li>
+            @endforeach
+          </ul>
+        </section>
 
-        <h2>チャレンジ中STEP</h2>
-        <ul>
-          <li>STEP1</li>
-          <li>STEP2</li>
-        </ul>
+        <section class="u-mb-4">
+          <h2>チャレンジ中STEP</h2>
+          <div>
+            @if(!empty($challenge_steps[0]->id))
+            <ul>
+              @foreach($challenge_steps as $challenge_step)
+                <li class="">
+                  <a href="/steps/{{ $challenge_step->id }}">
+                    {{ $challenge_step['title'] }}
+                  </a>
+                  （ {{ $challenge_step['challenges'][0]['current_step']-1 }} / {{ count($challenge_step['childSteps']) }} STEP完了 ）
+                </li>
+              @endforeach
+              </ul>
+            @else
+              <div>
+                <p class="u-mb-3">チャレンジしているSTEPはありません。</p>
+                <a href="{{ route('steps') }}">チャレンジするSTEPを探す</a>
+              </div>
+            @endif
+          </div>
+        </section>
       </div>
 
-      <div class="menus-container col-md-4">
+      <div class="c-menus-container">
         <h2>ユーザー情報</h2>
         <ul class="u-bg-white">
           <li><a href="{{ route('profile.edit') }}">プロフィール編集</a></li>
