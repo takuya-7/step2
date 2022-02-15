f236e8356e09d27dbaf73fbb1028e7efb9be59f6
takuya-7 <ts.procd@gmail.com>
2022-02-15 18:51:17 +0900

    子STEPクリア処理追加

--- app/Http/Controllers/ChallengesController.php
+++ app/Http/Controllers/ChallengesController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Models\Challenge;
 use App\Models\Step;
+use App\Models\ChildStep;
 use Illuminate\Support\Facades\Auth;
 
 class ChallengesController extends Controller
@@ -26,5 +27,27 @@ class ChallengesController extends Controller
         $challenge = Challenge::where('step_id', $step->id)->where('user_id', $user)->first();
         $challenge->delete();
         return back();
+    }
+
+    // 子STEPクリア
+    public function clear(Step $step, Request $request){
+        $user = Auth::user()->id;
+        $challenge = Challenge::where('step_id', $step->id)->where('user_id', $user)->first();
+        $challenge->current_step = $challenge->current_step + 1;
+
+        // 子ステップ取得
+        $child_steps = ChildStep::where('step_id', $step->id)->get();
+
+        // 子STEPを全てクリアしたとき（$challenge->current_step > 総子STEP数）
+        if($challenge->current_step > count($child_steps)){
+            $challenge->clear_flg = 1;
+            $challenge->save();
+            // マイページへ遷移
+            return redirect(route('mypage'));
+        }
+        $challenge->save();
+
+        // 次の子STEPページへ遷移
+        return redirect(route('steps.showChild', ['id' => $step->id, 'order' => $challenge->current_step]));
     }
 }
