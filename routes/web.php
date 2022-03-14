983c0eaf07cd3be85878c938db2baf18701d4471
takuya-7 <ts.procd@gmail.com>
2022-03-14 22:36:28 +0900

    使用するController宣言。不要なルーティングを削除

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\TopController;
+use App\Http\Controllers\UsersController;
+use App\Http\Controllers\StepsController;
+use App\Http\Controllers\ChallengesController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,46 +19,48 @@ use Illuminate\Support\Facades\Route;
 */
 
 // トップページ
-Route::get('/', [App\Http\Controllers\TopController::class, 'index'])->name('top');
+Route::get('/', [TopController::class, 'index'])->name('top');
+
+Auth::routes();
 
 // ユーザー認証必要ページ
 Route::group(['middleware' => 'auth'], function() {
     // マイページ
-    Route::get('/mypage', [App\Http\Controllers\UsersController::class, 'mypage'])->name('mypage');
+    Route::get('/mypage', [UsersController::class, 'mypage'])->name('mypage');
     // プロフィール登録・編集
-    Route::get('/profile/edit', [App\Http\Controllers\UsersController::class, 'edit'])->name('profile.edit');
+    Route::get('/profile/edit', [UsersController::class, 'edit'])->name('profile.edit');
     // プロフィール更新処理
-    Route::patch('/profile/edit', [App\Http\Controllers\UsersController::class, 'update'])->name('profile.update');
+    Route::patch('/profile/edit', [UsersController::class, 'update'])->name('profile.update');
 
     // STEP投稿画面
-    Route::get('/steps/new', [App\Http\Controllers\StepsController::class, 'new'])->name('steps.new');
+    Route::get('/steps/new', [StepsController::class, 'new'])->name('steps.new');
     // STEP登録処理
-    Route::post('/steps', [App\Http\Controllers\StepsController::class, 'create'])->name('steps.create');
+    Route::post('/steps', [StepsController::class, 'create'])->name('steps.create');
     // STEP編集画面
-    Route::get('/steps/{id}/edit', [App\Http\Controllers\StepsController::class, 'edit'])->name('steps.edit');
+    Route::get('/steps/{id}/edit', [StepsController::class, 'edit'])->name('steps.edit');
     // STEP更新処理
-    Route::post('/steps/{id}/edit', [App\Http\Controllers\StepsController::class, 'update'])->name('steps.update');
+    Route::post('/steps/{id}/edit', [StepsController::class, 'update'])->name('steps.update');
+    // STEP削除処理
+    Route::post('/steps/{id}/delete', [StepsController::class, 'destroy'])->name('steps.delete');
 
     // STEPチャレンジ登録・解除
-    Route::get('/steps/challenge/{step}', [App\Http\Controllers\ChallengesController::class, 'challenge'])->name('challenge');
-    Route::get('/steps/unchallenge/{step}', [App\Http\Controllers\ChallengesController::class, 'unchallenge'])->name('unchallenge');
+    Route::get('/steps/challenge/{step}', [ChallengesController::class, 'challenge'])->name('challenge');
+    Route::get('/steps/unchallenge/{step}', [ChallengesController::class, 'unchallenge'])->name('unchallenge');
 
     // 子STEPクリア
-    Route::get('/steps/clear/{step}', [App\Http\Controllers\ChallengesController::class, 'clear'])->name('clear');
+    Route::get('/steps/clear/{step}', [ChallengesController::class, 'clear'])->name('clear');
 });
 
 // STEP一覧
-Route::get('/steps', [App\Http\Controllers\StepsController::class, 'index'])->name('steps');
+Route::get('/steps', [StepsController::class, 'index'])->name('steps');
 // STEP詳細
-Route::get('/steps/{id}', [App\Http\Controllers\StepsController::class, 'show'])->name('steps.show');
+Route::get('/steps/{id}', [StepsController::class, 'show'])->name('steps.show');
 // 子STEP詳細
-Route::get('/steps/{id}/{order}', [App\Http\Controllers\StepsController::class, 'showChild'])->name('steps.showChild');
+Route::get('/steps/{id}/{order}', [StepsController::class, 'showChild'])->name('steps.showChild');
 
 // サイトマップ
 Route::get('/sitemap', function () {
     return view('sitemap');
 })->name('sitemap');
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
