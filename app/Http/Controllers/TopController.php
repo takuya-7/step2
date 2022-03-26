f8338711d3178d204d88a3e387df236ea8002e0b
takuya-7 <ts.procd@gmail.com>
2022-03-26 21:11:15 +0900

    ログイン状態でトップページにアクセスした時、マイページへ遷移させるように変更

--- app/Http/Controllers/TopController.php
+++ app/Http/Controllers/TopController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 use App\Models\Category;
 use App\Models\Step;
@@ -10,6 +11,10 @@ use App\Models\Step;
 class TopController extends Controller
 {
     public function index(){
+        // ログインしている場合、マイページへリダイレクトする
+        if(Auth::check()){
+            return redirect('/mypage');
+        }
         $categories = Category::all();
         $steps = Step::all();
 
