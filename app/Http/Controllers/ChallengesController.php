bfd1520954b058e6a21122d9d4ed55d6824c82f7
takuya-7 <ts.procd@gmail.com>
2022-04-06 01:57:31 +0900

    チャレンジボタン、クリアボタンコンポーネント化に伴い、引数でstep_idを受けるように変更

--- app/Http/Controllers/ChallengesController.php
+++ app/Http/Controllers/ChallengesController.php
@@ -12,31 +12,31 @@ use Illuminate\Support\Facades\Auth;
 class ChallengesController extends Controller
 {
     // STEPチャレンジ登録
-    public function challenge(Step $step, Request $request){
+    public function challenge($step_id){
         $challenge = New Challenge();
-        $challenge->step_id = $step->id;
+        $challenge->step_id = $step_id;
         $challenge->user_id = Auth::user()->id;
         $challenge->save();
 
         // 子STEP1のページへ遷移
-        return redirect(route('steps.showChild', ['id' => $step->id, 'order' => 1]));
+        return redirect(route('steps.showChild', ['id' => $step_id, 'order' => 1]));
     }
     // STEPチャレンジ削除
-    public function unchallenge(Step $step, Request $request){
+    public function unchallenge($step_id){
         $user = Auth::user()->id;
-        $challenge = Challenge::where('step_id', $step->id)->where('user_id', $user)->first();
+        $challenge = Challenge::where('step_id', $step_id)->where('user_id', $user)->first();
         $challenge->delete();
         return back();
     }
 
     // 子STEPクリア
-    public function clear(Step $step, Request $request){
+    public function clear($step_id){
         $user = Auth::user()->id;
-        $challenge = Challenge::where('step_id', $step->id)->where('user_id', $user)->first();
+        $challenge = Challenge::where('step_id', $step_id)->where('user_id', $user)->first();
         $challenge->current_step = $challenge->current_step + 1;
 
         // 子ステップ取得
-        $child_steps = ChildStep::where('step_id', $step->id)->get();
+        $child_steps = ChildStep::where('step_id', $step_id)->get();
 
         // 子STEPを全てクリアしたとき（$challenge->current_step > 総子STEP数）
         if($challenge->current_step > count($child_steps)){
@@ -48,6 +48,6 @@ class ChallengesController extends Controller
         $challenge->save();
 
         // 次の子STEPページへ遷移
-        return redirect(route('steps.showChild', ['id' => $step->id, 'order' => $challenge->current_step]));
+        return redirect(route('steps.showChild', ['id' => $step_id, 'order' => $challenge->current_step]));
     }
 }
