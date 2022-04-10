d1aabb6aebf9b865539b2d9bf13c4e6b47807e07
takuya-7 <ts.procd@gmail.com>
2022-04-10 18:25:57 +0900

    改行削除

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -9,16 +9,13 @@
         <div class="l-container">
           <div class="l-content">
             <div class="l-inner-container">
-
               <h2>サイトマップ</h2>
-
               <ul>
                 <li><a href="/">HOME</a></li>
                 <li><a href="{{ route('register') }}">ユーザー登録</a></li>
                 <li><a href="{{ route('login') }}">ログイン</a></li>
                 <li><a href="{{ route('steps') }}">STEP一覧</a></li>
               </ul>
-              
             </div>
           </div>
         </div>
